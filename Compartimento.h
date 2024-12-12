51cdaa29362bafa97dbbd53249893b8542ca2982
Érick Vinícius <erickvinicius19675@gmail.com>
2024-12-12 19:21:11 -0300

    Add files via upload

--- Compartimento.h
+++ Compartimento.h
@@ -13,5 +13,8 @@ void inicializarListaComp(ListaRochas* lista);
 void insere_Rocha(ListaRochas* lista, float valor, float peso, int usada);
 void remove_Rocha(ListaRochas* lista, int indice);
 void ImprimeCompartimento(ListaRochas* lista);
+void avaliaCombinacao(ListaRochas* lista, int combinacao, float* valorTotal, float* pesoTotal);
+void melhor_rocha(ListaRochas* lista);
+void marcaRochasUsadas(ListaRochas* lista, int combinacao);
 
 #endif
